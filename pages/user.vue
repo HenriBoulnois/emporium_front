75a75c5870ae1aa6eedf383a183f399f250da49a
HenriBoulnois <boulnois.henri@gmail.com>
2023-04-11 16:03:11 +0200

    ✨ First try sidebar

---
+++ pages/user.vue
@@ -0,0 +1,12 @@
+<template>
+  <SideBar />
+</template>
+
+<script>
+import SideBar from '~/components/SideBar.vue';
+
+export default {
+    name: 'UserPage',
+    components: { SideBar }
+}
+</script>
