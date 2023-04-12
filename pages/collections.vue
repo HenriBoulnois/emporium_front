7240d432a84f73201a55cecc2f2df9e29bc43f74
HenriBoulnois <boulnois.henri@gmail.com>
2023-04-12 10:32:47 +0200

    :sparkles: Nav between pages

---
+++ pages/collections.vue
@@ -0,0 +1,12 @@
+<template>
+  <SideBar />
+</template>
+
+<script>
+import SideBar from '~/components/SideBar.vue';
+
+export default {
+  name: 'CollectionsPage',
+  components: { SideBar }
+};
+</script>
