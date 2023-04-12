b809832f76c3081c620b3a71f609e6932d2b1294
Tarskan <55380090+Tarskan@users.noreply.github.com>
2023-04-12 11:06:18 +0200

    ✨add pages for error and footer

---
+++ components/EmFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="p-4  bg-gray-50 dark:bg-gray-800 text-center">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+          2023 - 2024 © Emporium
+        </div>
+        <div class="col-md-6">
+          <div class="text-md-right footer-links d-none d-sm-block text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <a href="">A propos de nous</a>
+            <a href="">Aide</a>
+            <a href="">Nous contacter</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'EmFooter'
+}
+</script>
+
+<style scoped></style>
---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="container m-auto flex-row flex flex-wrap justify-center items-center"
+  >
+    <div class="w-2/4">
+      <ex-h-3 class="mt-0 mb-8 text-md text-center">
+        Mince la page n'existe pas...
+      </ex-h-3>
+      <div class="text-center">
+        <button @click="$router.push('/')">
+          Retour
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
