a9d8c0c44d32e6781b80fd9aa894566b683eda7d
HenriBoulnois <boulnois.henri@gmail.com>
2023-04-17 17:36:33 +0200

    :sparkles: Search input and search page

---
+++ pages/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-white">Query: {{ query }}</div>
+</template>
+
+<script>
+export default {
+  name: 'SearchPage',
+  computed: {
+    query () {
+      return this.$route.query.q;
+    }
+  },
+  watchQuery: ['q']
+};
+</script>
