b08a2ccbeb617330d1d4bb8abc93ce5e5a74afdf
HenriBoulnois <boulnois.henri@gmail.com>
2023-04-24 15:16:07 +0200

    :sparkles: new specific user page

--- pages/user.vue
+++ pages/user.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    Page user
+  <div class="grid grid-rows-2">
+    <div class="grid grid-cols-3 place-content-center">
+      <img :src="user.image">
+      <div class="col-span-2 grid grid-cols-1 list-none place-content-center">
+        <li>
+          <a class="m-7">{{ user.firstName }}</a>
+        </li>
+        <li>
+          <a class="">{{ user.lastName }}</a>
+        </li>
+        <li>
+          <a>{{ user.birthDate }}</a>
+        </li>
+        <li>
+          <a>{{ user.university }}</a>
+        </li>
+      </div>
+    </div>
+    <div>Collection zone</div>
   </div>
 </template>
 
 <script>
-
 export default {
-    name: 'UserPage'
-}
+  name: 'UserPage',
+  data () {
+    return {
+      user: []
+    };
+  },
+  async fetch () {
+    // exact user
+    const response = await this.$axios.$get(
+      'https://dummyjson.com/users/search?q=' + this.$route.query.q
+    );
+    this.user = response.users[0];
+  },
+  watch: {
+    '$route.query': '$fetch'
+  }
+};
 </script>
