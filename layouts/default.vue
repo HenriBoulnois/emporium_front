ae04733200952dddaa43ac7f3a2672e20fc378dc
HenriBoulnois <boulnois.henri@gmail.com>
2023-04-24 15:15:55 +0200

    :lipstick: Adding shadows to elements

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,8 @@
       <EmHeader />
       <div class="grid grid-cols-[auto_1fr]">
         <EmSideBar />
-        <div class="rounded-tl-lg bg-gray-600 pt-10 px-10">
-          <Nuxt class=" bg-gray-800 rounded-t-lg h-full" />
+        <div class="rounded-tl-lg bg-gray-600 pt-10 px-10 shadow-inner shadow-gray-700">
+          <Nuxt class=" bg-gray-800 rounded-t-lg h-full shadow-2xl shadow-gray-500" />
         </div>
       </div>
     </div>
