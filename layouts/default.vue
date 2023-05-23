da1357b95e24170e4af83d9ea9726dc5919b4ba4
HenriBoulnois <boulnois.henri@gmail.com>
2023-05-23 23:20:23 +0200

    :heavy_plus_sign: Add vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="h-screen bg-gray-900">
-    <div class="grid grid-rows-[auto_1fr] h-full">
-      <EmHeader />
-      <div class="grid grid-cols-[auto_1fr]">
-        <EmSideBar />
-        <div class="rounded-tl-lg bg-zinc-600 pt-10 px-10 shadow-inner shadow-gray-700">
-          <Nuxt class=" bg-gray-800 rounded-t-lg h-full shadow-2xl shadow-gray-500" />
+  <v-app>
+    <div class="h-screen bg-gray-900">
+      <div class="grid grid-rows-[auto_1fr] h-full">
+        <EmHeader />
+        <div class="grid grid-cols-[auto_1fr]">
+          <EmSideBar />
+          <div
+            class="rounded-tl-lg bg-zinc-600 pt-10 px-10 shadow-inner shadow-gray-700"
+          >
+            <Nuxt
+              class="bg-gray-800 rounded-t-lg h-full shadow-2xl shadow-gray-500"
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/vuetify', { theme: { disable: true } }]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
