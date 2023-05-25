e37edcde9f70c8b9b69d69775f3dee615b4a5090
HenriBoulnois <72384047+HenriBoulnois@users.noreply.github.com>
2023-05-25 18:08:15 +0200

    :rotating_light: Remove multi world component warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     semi: 0,
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'development' ? 'error' : 'off'
-
+    'no-debugger': process.env.NODE_ENV === 'development' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
