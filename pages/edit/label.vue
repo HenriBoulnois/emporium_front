9ae89e59569afb03a75b9dab6a367dec794a5c3e
HenriBoulnois <boulnois.henri@gmail.com>
2023-05-26 12:46:16 +0200

    :sparkles: Label can be edited

---
+++ pages/edit/label.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="grid">
+    <div class="grid grid-cols-3 bg-gray-700 shadow-inner rounded-lg h-fit">
+      <div class="grid bg-white p-4 rounded-l-lg h-full">
+        Liste des labels
+        <v-autocomplete
+          v-model="inputLabel"
+          :items="listType"
+          item-text="name"
+          return-object
+          label="Type"
+        />
+        <v-autocomplete
+          v-model="inputLabel"
+          :items="listAuteur"
+          item-text="name"
+          return-object
+          label="Auteur"
+        />
+        <v-autocomplete
+          v-model="inputLabel"
+          :items="listEditeur"
+          item-text="name"
+          return-object
+          label="Editeur"
+        />
+        <v-autocomplete
+          v-model="inputLabel"
+          :items="listSupport"
+          item-text="name"
+          return-object
+          label="Support"
+        />
+        <v-autocomplete
+          v-model="inputLabel"
+          :items="listGenre"
+          item-text="name"
+          return-object
+          label="Genre"
+        />
+      </div>
+      <div class="grid bg-white p-4 h-full rounded-r-lg drop-shadow-xl">
+        <div class="">
+          Formulaire de modification
+        </div>
+        {{ inputLabel }}
+        <a class="block text-gray-700 text-sm font-bold mb-2"> Nouveau nom :</a>
+        <input
+          v-model="inputNewName"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Nouveau nom"
+        >
+        <div class="bg-red-300" @click="submitChange()">
+          Submit change
+        </div>
+      </div>
+      <div class="grid p-4 h-full">
+        <div class="">
+          Visualisation des changements
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditLabelPage',
+  data () {
+    return {
+      listType: undefined,
+      listAuteur: undefined,
+      listEditeur: undefined,
+      listSupport: undefined,
+      listGenre: undefined,
+      inputLabel: undefined,
+      inputNewName: undefined
+    };
+  },
+  async fetch () {
+    await this.$axios
+      .$get('https://emporiumback.fly.dev/type')
+      .then((response) => {
+        this.listType = response;
+      });
+    await this.$axios
+      .$get('https://emporiumback.fly.dev/auteur')
+      .then((response) => {
+        this.listAuteur = response;
+      });
+    await this.$axios
+      .$get('https://emporiumback.fly.dev/editeur')
+      .then((response) => {
+        this.listEditeur = response;
+      });
+    await this.$axios
+      .$get('https://emporiumback.fly.dev/support')
+      .then((response) => {
+        this.listSupport = response;
+      });
+    await this.$axios
+      .$get('https://emporiumback.fly.dev/genre')
+      .then((response) => {
+        this.listGenre = response;
+      });
+  },
+  methods: {
+    submitChange () {
+      if (this.inputNewName !== undefined && this.inputLabel !== undefined) {
+        if (this.inputLabel.idType !== undefined) {
+          const newLabelName = {
+            id: this.inputLabel.idType,
+            name: this.inputNewName
+          };
+          this.$axios
+            .$put('https://emporiumback.fly.dev/type', newLabelName)
+            .then(() => {
+              this.inputNewName = undefined;
+            });
+        }
+        if (this.inputLabel.idAuteur !== undefined) {
+          const newLabelName = {
+            id: this.inputLabel.idAuteur,
+            name: this.inputNewName
+          };
+          this.$axios
+            .$put('https://emporiumback.fly.dev/auteur', newLabelName)
+            .then(() => {
+              this.inputNewName = undefined;
+            });
+        }
+        if (this.inputLabel.idGenre !== undefined) {
+          const newLabelName = {
+            id: this.inputLabel.idGenre,
+            name: this.inputNewName
+          };
+          this.$axios
+            .$put('https://emporiumback.fly.dev/genre', newLabelName)
+            .then(() => {
+              this.inputNewName = undefined;
+            });
+        }
+        if (this.inputLabel.idEditeur !== undefined) {
+          const newLabelName = {
+            id: this.inputLabel.idEditeur,
+            name: this.inputNewName
+          };
+          this.$axios
+            .$put('https://emporiumback.fly.dev/editeur', newLabelName)
+            .then(() => {
+              this.inputNewName = undefined;
+            });
+        }
+        if (this.inputLabel.idSupport !== undefined) {
+          const newLabelName = {
+            id: this.inputLabel.idSupport,
+            name: this.inputNewName
+          };
+          this.$axios
+            .$put('https://emporiumback.fly.dev/support', newLabelName)
+            .then(() => {
+              this.inputNewName = undefined;
+            });
+        }
+      }
+    }
+  }
+};
+</script>
