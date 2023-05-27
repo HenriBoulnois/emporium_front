1fb4bbef860f19dc52edf07ccdc85b8be7494638
HenriBoulnois <boulnois.henri@gmail.com>
2023-05-28 01:33:59 +0200

    :sparkles: Reworking login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
     <div class="grid grid-cols-[1fr_auto_1fr]">
-      <div>gauche</div>
-      <div class="bg-blue-500 p-5 m-5">
-        <button @click="$auth.loginWith('auth0')">
-          Log in
-        </button>
-        <button v-if="$auth.loggedIn" @click="$auth.logout()">
-          Logout
-        </button>
-        <a v-if="$auth.loggedIn">{{ $auth.user.email }}</a>
+      <div />
+      <div class="grid grid-rows-[auto_auto_auto] bg-white p-5 rounded-lg place-items-center">
+        <div v-if="!$auth.loggedIn">
+          Emporium utilise le service d'authentification Auth0, pour vous
+          connecter cliquer sur ce boutton.
+        </div>
+        <div
+          v-if="!$auth.loggedIn"
+          class="w-fit text-center text-white bg-gray-800 cursor-pointer hover:bg-gray-900 font-medium rounded-lg text-sm m-4 px-4 py-2"
+          @click="$auth.loginWith('auth0')"
+        >
+          Se connecter
+        </div>
+        <div>
+          Il faudra ensuite compléter votre profil pour profiter de toutes les
+          fonctionnalités du site comme :
+          <div>- L'ajout et la modification d'oeuvre</div>
+          <div>- L'ajout et la modification de label</div>
+          <div>- La personnalisation du profil</div>
+          <div>- La gestion de sa collection personnelle avec des favoris !</div>
+        </div>
+        <div
+          v-if="$auth.loggedIn"
+          class="w-fit text-center text-white bg-gray-800 cursor-pointer hover:bg-gray-900 font-medium rounded-lg text-sm m-4 px-4 py-2"
+          @click="$auth.logout()"
+        >
+          Se déconnecter
+        </div>
       </div>
-      <div>droite</div>
+      <div />
     </div>
   </div>
 </template>
